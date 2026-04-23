ebb80473fb3be4e676e5c0acc8c2e68bd6aa94c6
aanmol18 <anmoly683@gmail.com>
2026-04-23 13:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 800; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => {
+        const color = getRandomColor();
+        square.style.backgroundColor = color;
+
+        setTimeout(() => {
+            square.style.backgroundColor = 'rgb(29, 29, 29)';
+        }, 1000);
+    });
+
+    container.appendChild(square);
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
